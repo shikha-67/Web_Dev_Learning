dd9c06fb10fdbedc412beebd0a65e31ad16bc644
shikha-67 <103561632+shikha-67@users.noreply.github.com>
2026-04-04 11:14:18 +0530

    Data Types folder created

---
+++ JavaScript/Data Types/practice.js
@@ -0,0 +1,16 @@
+var age =25;
+console.log(age,typeof(age));
+
+//2nd
+var name="Alice";
+console.log(name,typeof(name));
+
+//3rd
+var isactive=true;
+console.log(isactive,typeof(isactive));
+
+//4th
+var user="Null";
+var x;
+console.log(user,x);
+console.log(typeof(user),typeof(x));
