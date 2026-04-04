662d880362a9ea434250eeb682f72b86bd88266e
shikha-67 <103561632+shikha-67@users.noreply.github.com>
2026-04-04 11:36:14 +0530

    BigInt concept learned

--- JavaScript/Data Types/_3rd_BigInt.js
+++ JavaScript/Data Types/_3rd_BigInt.js
@@ -2,11 +2,11 @@ const int=234598585745874n;
 console.log(int);
 
 const int1=BigInt(23354556672234);
-console.log(int1);
+console.log(int1); //23354556672234n
 console.log(typeof(1n)); //BigInt
 
 console.log(typeof(BigInt("1"))); //BigINT
-console.log(Number.isSafeInteger(4559285829557892222424222n)); //false (greater than the size of BigInt)
+console.log(Number.isSafeInteger(4559285829557892222424222n)); //false becuase its > (2^53)-1
 console.log(Number.isSafeInteger(9007199254740991n)); //false 
 console.log(Number.isSafeInteger(1n)); //false
 console.log(2n**5n);  // 32n (2^5=32n)
@@ -14,4 +14,4 @@ console.log(BigInt(2)**BigInt(54)); // 18014398509481984n (2^54=1801439850948198
 let big1=2n**54n; //2^54
 let big2=BigInt(2)**BigInt(54);
 console.log(big1===big2); //true
-console.log(null==undefined);
+
